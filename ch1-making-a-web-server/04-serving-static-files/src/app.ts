11d28a7b659a48652a58ec0ff50b451d1e99fef2
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-12 16:49:08 +0800

    [ENHANCE] Complete 04-serving-static-files with promise version and no promise version

--- ch1-making-a-web-server/04-serving-static-files/src/app.ts
+++ ch1-making-a-web-server/04-serving-static-files/src/app.ts
@@ -2,20 +2,17 @@ import * as http from 'http';
 import * as path from 'path';
 import * as fs from 'fs';
 
+import * as Promise from 'bluebird';
+
 
 /**
- * type
+ * const
  */
 interface IRoute {
     id: string,
     route: string,
     output?: string | (() => string),
 }
-
-
-/**
- * const
- */
 const routes: Array<IRoute> = [
     { id: '1', route: '', output: 'Woohoo!' },
     { id: '2', route: 'about', output: 'A simple routing with Nodeexample' },
@@ -27,19 +24,87 @@ const routes: Array<IRoute> = [
 ];
 
 
+interface IMimeTypes {
+    ".js": string,
+    ".html": string,
+    ".css": string,
+}
+const mimeTypes: IMimeTypes = {
+    '.js': 'text/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css',
+};
+
+
 /**
  * server
  */
+interface IPromiseError extends NodeJS.ErrnoException {
+    cause: NodeJS.ErrnoException,
+}
+const server = http.createServer((req: http.IncomingMessage,
+                                  res: http.ServerResponse) => {
+    // Local variables
+    let lookup = path.basename(decodeURI(req.url)) || 'index.html';
+    let filePath = `${__dirname}/../content/${lookup}`
+
+    // Wrap fs.access in Promise
+    let access = Promise.promisify(fs.access)
+    access(filePath).then(() => {
+        // the path is valid
+        console.log(`${filePath} doesn't exist`);
+
+        // Wrap fs.readFile in Promise
+        let readFile = Promise.promisify(fs.readFile);
+        readFile(filePath).then((data: Buffer) => {
+            // Response with buffer
+            let headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
+            res.writeHead(200, headers);
+            res.end(data);
+        });
+    }).catch((err: IPromiseError) => {
+        // Log invalid path
+        console.log(`${filePath} doesn't exist`);
+
+        // Response with ServerError
+        res.writeHead(500);
+        res.end('ServerError!');
+        return;
+    })
+});
+
+
+/**
+ * No promise version
+ */
+/*
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     // Local variables
     let lookup = path.basename(decodeURI(req.url)) || 'index.html';
     let filePath = `${__dirname}/../content/${lookup}`
 
     // fs.exists had deprecated. Please use fs.stat() or fs.access()
-    fs.access(filePath, fs.R_OK, (err: NodeJS.ErrnoException) => {
+    fs.access(filePath, (err: NodeJS.ErrnoException) => {
+
+        // Check file
         console.log(err ? `${filePath} doesn't exist` : `${filePath} is there`);
-    })
+
+        // Read file when existed
+        fs.readFile(filePath, (err: NodeJS.ErrnoException, data: Buffer) => {
+            // Content not found
+            if (err) { res.writeHead(500); res.end('ServerError!'); return; }
+
+            // Content can be found
+            let headers = {
+                'Content-type': mimeTypes[path.extname(lookup)]
+            };
+            res.writeHead(200, headers);
+            res.end(data);
+        });
+    });
+
 });
+*/
 
 server.listen(8080, () => {
     console.log(`[INFO] Create http server: 127.0.0.1:8080 / localhost:8080`)
