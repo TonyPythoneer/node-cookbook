0075c1fe8828b6267ccba6beb809ffd17075511b
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-18 16:42:53 +0800

    [EDIT] Rename

---
+++ ch2-exploring-the-http-object/02-using-formidable-to-accept-all-post-data/src/app.ts
@@ -0,0 +1,45 @@
+import * as http from 'http';
+import * as fs from 'fs';
+
+import * as formidable from 'formidable';
+
+
+
+/**
+ *  const
+ */
+const form = fs.readFileSync('content/form.html');
+const maxData = 2 * 1024 * 1024; //2mb
+
+/**
+ * server
+ */
+const server = http.createServer((req, res) => {
+    switch (req.method) {
+        case "GET":
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(form);
+            break;
+        case "POST":
+            let incoming = new formidable.IncomingForm();
+            incoming.uploadDir = 'uploads';
+
+            // create the dir if unexisted
+            if (!fs.existsSync(incoming.uploadDir)) fs.mkdirSync(incoming.uploadDir);
+
+            // event listener
+            incoming.on('file', (field, file) => {
+                if (!file.size) { return; }
+                res.write(`${file.name} received\n`);
+            }).on('field', (field, value) => {
+                // unreacted: the module is long time to mantain
+                res.write(`${field} : ${value} \n`);
+            }).on('end', () => {
+                res.end('All files received');
+            });
+
+            // parse request
+            incoming.parse(req);
+            break;
+    }
+}).listen(8080);
