5f9f93cfd3054e6bcc3a729fa096d6383e461693
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-23 17:43:25 +0800

    [ENHANCE] Complete 00-converting-an-object-to-json-and-back

---
+++ ch03-working-with-data-serialization/00-converting-an-object-to-json-and-back/src/app.ts
@@ -0,0 +1,19 @@
+import * as http from 'http';
+
+import PROFILES from './profiles';
+
+
+/**
+ * server
+ */
+const server = http.createServer((req, res) => {
+    let profiles = JSON.stringify(PROFILES).replace(/name/g, 'fullname');
+    profiles = JSON.parse(profiles);
+    profiles.felix.fullname = "Felix Geisendörfer";
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(profiles));
+}).listen(8080);
+
+server.on('clientError', (err, socket) => {
+    if (err) console.log(err.toString()); // Catch the exception of req.destroy
+});
