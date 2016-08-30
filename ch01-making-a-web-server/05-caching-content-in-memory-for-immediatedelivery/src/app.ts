670c7dace3ab180993a7d21890bb98934d5cb168
TonyPythoneer <tonypythoneer@gmail.com>
2016-08-30 17:25:24 +0800

    [ENHANCE] Used promise module and hoisting skill to enhance readility

--- ch01-making-a-web-server/05-caching-content-in-memory-for-immediatedelivery/src/app.ts
+++ ch01-making-a-web-server/05-caching-content-in-memory-for-immediatedelivery/src/app.ts
@@ -23,7 +23,6 @@ const routes: Array<IRoute> = [
     },
 ];
 
-
 interface IMimeTypes {
     ".js": string,
     ".html": string,
@@ -35,47 +34,31 @@ const mimeTypes: IMimeTypes = {
     '.css': 'text/css',
 };
 
-
-/**
- * cache
- */
 interface ICache {
-    [key: string] : {
+    [key: string]: {
         content: Buffer
     }
 }
 const cache: ICache = {};
-function cacheAndDeliver(f: string,
-                         cb: (err:NodeJS.ErrnoException, data: Buffer) => void) {
+
+/**
+ * functions
+ */
+function cacheAndDeliver(f: string, cb: (err:NodeJS.ErrnoException, data: Buffer) => void) {
     if (!cache[f]) {
-        fs.readFile(f, (err: NodeJS.ErrnoException, data: Buffer) => {
-            // Cache the data when it's not in cache
-            if (!err) cache[f] = { content: data };
-            cb(err, data);
-        });
+        fs.readFile(f, deliverCacheBuffer);
         return;
     }
     console.log(`loading ${f} from cache`);
     cb(null, cache[f].content);
-}
-function cacheAndDeliverWithPromise(f: string,
-                                    cb: (err: NodeJS.ErrnoException, data: Buffer) => void) {
-    if (!cache[f]) {
-        let readFile = Promise.promisify(fs.readFile);
-        readFile(f).then((data: Buffer) => {
-            // -- SUCESSFUL --
-            cache[f] = { content: data };
-            cb(null, data);
-        }).catch((err) => {
-            // -- FAIL --
-            cb(err, null);
-        });
-    }
-    // return cache when existed
-    console.log(`loading ${f} from cache`);
-    cb(null, cache[f].content);
-}
 
+    /////// hoisted functions
+
+    function deliverCacheBuffer(err: NodeJS.ErrnoException, data: Buffer) {
+        if (!err) cache[f] = { content: data };
+        cb(err, data);
+    }
+}
 
 /**
  * server
@@ -83,7 +66,7 @@ function cacheAndDeliverWithPromise(f: string,
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     // Local variables
     let lookup = path.basename(decodeURI(req.url)) || 'index.html';
-    let filePath = `${__dirname}/../content/${lookup}`
+    let filePath = `./content/${lookup}`
 
     // fs.exists had deprecated. Please use fs.stat() or fs.access()
     fs.access(filePath, (err: NodeJS.ErrnoException) => {
@@ -113,45 +96,36 @@ const server = http.createServer((req: http.IncomingMessage, res: http.ServerRes
     console.log(`[INFO] Create http server: 127.0.0.1:8080 / localhost:8080`)
 });
 
-
-interface IPromiseError extends NodeJS.ErrnoException {
-    cause: NodeJS.ErrnoException,
-}
-const serverWithPromise = http.createServer((req: http.IncomingMessage,
-    res: http.ServerResponse) => {
+const serverWithPromise = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     // Local variables
     let lookup = path.basename(decodeURI(req.url)) || 'index.html';
-    let filePath = `${__dirname}/../content/${lookup}`
+    let filePath = `./content/${lookup}`
 
-    // Wrap fs.access in Promise
-    let access = Promise.promisify(fs.access)
-    access(filePath).then(() => {
-        // -- SUCESSFUL --
-        // the path is valid
-        console.log(`Exist!`);
+    let access = Promise.promisify(fs.access);
+    access(filePath)
+        .then(cacheHandle)
+        .then(responseHandle)
+        .catch(errorHandle);
 
-        // Wrap cacheAndDeliverWithPromise in Promise
-        let cacheFunc = Promise.promisify(cacheAndDeliverWithPromise);
-        cacheFunc(filePath).then((data: Buffer) => {
-            // -- SUCESSFUL --
-            // Content can be found
-            let headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
-            res.writeHead(200, headers);
-            res.end(data);
-        }).catch((err) => {
-            // -- FAIL --
-            // Content not found
-            res.writeHead(500); res.end('ServerError!'); return;
-        })
-    }).catch((err: IPromiseError) => {
-        // -- FAIL --
-        // Log invalid path
-        console.log(`${filePath} doesn't exist`);
+    //////// hoisted functions
 
-        // Response with 404
-        res.writeHead(404);
-        res.end('Page not Found!');
-    })
+    function cacheHandle() {
+        let cachePromise = Promise.promisify(cacheAndDeliver);
+        return cachePromise(filePath);
+    }
+
+    function responseHandle(data: Buffer) {
+        let headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
+        res.writeHead(200, headers);
+        res.end(data);
+    }
+
+    function errorHandle(err) {
+        res.writeHead(500);
+        res.end('ServerError!');
+        return;
+    }
+
 }).listen(8081, () => {
     console.log(`[INFO] Create http server with promise version: 127.0.0.1:8081 / localhost:8081`)
 });
