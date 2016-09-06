d46c24bf943ab1049b31917322c268ab246292b6
TonyPythoneer <tonypythoneer@gmail.com>
2016-09-06 19:42:39 +0800

    [ENHANCE] Finish some process workflows

--- scripts/PackageEditor/script.ts
+++ scripts/PackageEditor/script.ts
@@ -3,22 +3,52 @@ import * as path from 'path';
 
 import * as Promise from 'bluebird';
 
-
-const CONTENTS_DIR = '../';
+import fsp from './fsp';
+import * as config from './config';
 
 
 function main() {
-    let readdir = Promise.promisify(fs.readdir);
-    readdir(CONTENTS_DIR)
-        .then(getRelativePathOfFiles)
+    fsp.readdirAsync(config.CONTENTS_DIR)
+        .map(joinContentsDir)
+        .filter(filterAndGetDirectories)
+        .filter(getChapters)
+        .map(readChapterDir)
+        .then((filenames) => {
+            console.log(filenames);
+        })
 
     ////// hoisted functions
 
-    function getRelativePath(files: Array<string>) {
-        return files.map((f: string) => path.join(CONTENTS_DIR, f));
+    function joinContentsDir(filename: string) {
+        return path.join(config.CONTENTS_DIR, filename);
     }
 
-    function getDirectories(files: Array<string>) {
+    function filterAndGetDirectories(filename: string) {
+        return fsp.statAsync(filename)
+            .then((stat) => stat.isDirectory())
+            .catch(() => false)
+    }
+
+    function getChapters(filename: string) {
+        let re = new RegExp(config.CHAPTER_PATTERN);
+        let basename = path.basename(filename);
+        let isChapter = Boolean(re.exec(basename));
+        return isChapter;
+    }
+
+    function readChapterDir(filename: string) {
+        return fsp.readdirAsync(filename)
+    }
+
+    function readSectionDir(filename: string) {
+
+    }
+
+    function filterAndGetPackagefile(filename: string) {
+
+    }
+
+    function editPackagefile(filename) {
 
     }
 
