10bc55daaeb813f571640eb205f03865c0250345
TonyPythoneer <tonypythoneer@gmail.com>
2016-09-19 03:13:59 +0800

    [ENHANCE] Improve semantics code

--- scripts/PackageEditor/index.ts
+++ scripts/PackageEditor/index.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+
 import * as Promise from 'bluebird';
 
 import * as config from './config';
@@ -6,63 +8,66 @@ import * as promiseUtil from './lib/promise-util';
 
 
 function main() {
-    fsp.readdirAsync(config.CONTENTS_DIR)
+    Promise.resolve(config.CONTENTS_DIR)
         .then(collectChapterDirs)
-        .then(readChapterDirs)
-        .then((filenames) => {
-            console.log(filenames);
-        })
-
-        /*
-        Current process:
-            { '../../ch01-making-a-web-server':
-               [ '../../ch01-making-a-web-server/00-setting-up-a-router',
-                 '../../ch01-making-a-web-server/01-simple-multilevel-routing',
-                 '../../ch01-making-a-web-server/02-simple-multilevel-routing-2',
-                 '../../ch01-making-a-web-server/03-parsing-the-querystring-module',
-                 '../../ch01-making-a-web-server/04-serving-static-files',
-                 '../../ch01-making-a-web-server/05-caching-content-in-memory-for-immediatedelivery',
-                 '../../ch01-making-a-web-server/06-reflecting-content-changes',
-                 '../../ch01-making-a-web-server/07-optimizing-performance-with-streaming',
-                 '../../ch01-making-a-web-server/08-protecting-against-process-memory-overruns',
-                 '../../ch01-making-a-web-server/09-securing-against-filesystem-hacking-exploits',
-                 '../../ch01-making-a-web-server/10-whitelisting' ],
-              '../../ch02-exploring-the-http-object':
-               [ '../../ch02-exploring-the-http-object/00-processing-post-data',
-                 '../../ch02-exploring-the-http-object/01-handling-file-upload',
-                 '../../ch02-exploring-the-http-object/02-using-formidable-to-accept-all-post-data',
-                 '../../ch02-exploring-the-http-object/03-uploading-files-via-put',
-                 '../../ch02-exploring-the-http-object/04-using-node-as-an-http-client',
-                 '../../ch02-exploring-the-http-object/05-implementing-download-throttling',
-                 '../../ch02-exploring-the-http-object/06-enabling-a-resume-request-from-broken-downloads',
-                 '../../ch02-exploring-the-http-object/07-implementing-download-throttling-2' ],
-              '../../ch03-working-with-data-serialization': [] }
-         */
+        .then(collectSectionDirs)
 
     ///// hoisted functions for promise
 
-    function collectChapterDirs(chapterDirs: string[]) {
-        return Promise.resolve(chapterDirs)
-            .map(promiseUtil.joinPathMapper(config.CONTENTS_DIR))
+    /**
+     * Read contentsDir to collect chapterDirs
+     * @param {string} contentsDir - contents directory
+     * @returns {string[]} chapterDirs - chapter directories
+     */
+    function collectChapterDirs(contentsDir: string) {
+        let joinContentsDirMapper = promiseUtil.joinPathMapper(contentsDir);
+        let matchChapterFilter = promiseUtil.matchReFilter(config.CHAPTER_PATTERN);
+        return fsp.readdirAsync(config.CONTENTS_DIR)
+            .map(joinContentsDirMapper)
             .filter(promiseUtil.getDirectoriesFilter)
-            .filter(promiseUtil.matchReFilter(config.CHAPTER_PATTERN))
+            .filter(matchChapterFilter)
     }
 
-    function readChapterDirs(chapterDirs: string[]) {
-        return Promise.resolve(chapterDirs).reduce(reducer, {})
+    /**
+     * Read contentsDir to collect sectionDirs
+     * @param {string[]} chapterDirs - chapter directories
+     * @returns {string[]} sectionDirs - section directories
+     */
+    function collectSectionDirs(chapterDirs: string[]) {
+        return Promise.reduce(chapterDirs, reducer, [])
 
         ///// hoisted functions
 
-        function reducer(prev: Object, current: string) {
+        function reducer(prev: string[], current: string, index: number) {
+            let joinChapterDirMapper = promiseUtil.joinPathMapper(current);
+            let matchSectionFilter = promiseUtil.matchReFilter(config.SECTION_PATTERN);
             return fsp.readdirAsync(current)
-                .map(promiseUtil.joinPathMapper(current))
-                .filter(promiseUtil.matchReFilter(config.SECTION_PATTERN))
-                .then((sectionDirs) => {
-                    prev[current] = sectionDirs;
-                    return prev
-                })
+                .map(joinChapterDirMapper)
+                .filter(matchSectionFilter)
+                .then((sectionDirs) => prev.concat(sectionDirs))
         }
 
+    }
+
+    function collectTargetFiles(nestContentDirs: string[][]) {
+        return Promise.map(nestContentDirs, mapper)
+
+        ///// hoisted functions
+
+        function mapper(sectionDirs: string[]) {
+            return Promise.map(sectionDirs, mapper2)
+        }
+
+        function mapper2(sectionDir: string) {
+            return fsp.readdirAsync(sectionDir)
+                .then(filenames => {
+                    let targetName = 'package.json';
+                    let index = filenames.indexOf(targetName);
+                    let targetFn = filenames[index];
+                    let targetFnDir = path.join(sectionDir, targetFn);
+                    return targetFnDir
+                })
+        }
     }
 
     ///// TODO
