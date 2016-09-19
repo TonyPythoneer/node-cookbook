d7b6bb1c79b8356db2b34b3a6aa8b232b66a8aaa
TonyPythoneer <tonypythoneer@gmail.com>
2016-09-19 13:16:34 +0800

    [ENHANCE] Clear up as object for config

--- scripts/PackageEditor/config.ts
+++ scripts/PackageEditor/config.ts
@@ -9,12 +9,20 @@ export const CONTENTS_DIR = '../../';
 export const CHAPTER_PATTERN = "^ch[0-9]{2}";
 export const SECTION_PATTERN = "^[0-9]{2}-";
 
+export const FILE_PATTERNS = {
+    CHAPTER: "^ch[0-9]{2}",
+    SECTION: "^[0-9]{2}-",
+};
+
+
 /**
  * Target files
  */
 export const TARGET_FILES = {
-    packageJson: "package.json",
-    typingsJson: "typings.json"
+    PACKAGE: "package.json",
+    TYPINGS: "typings.json"
 }
 
-export const TEMPLATE_PACKAGE = './package.sample.json';
+export const TEMPLATE_FILES = {
+    PACKAGE: './package.sample.json',
+};
