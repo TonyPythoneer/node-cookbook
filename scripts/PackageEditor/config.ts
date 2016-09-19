8941b3775acf327df73a953b95f2d0bd51a1590d
TonyPythoneer <tonypythoneer@gmail.com>
2016-09-19 12:11:24 +0800

    [ENHANCE] Add TARGET_FILES and TEMPLATE_PACKAGE

--- scripts/PackageEditor/config.ts
+++ scripts/PackageEditor/config.ts
@@ -8,3 +8,13 @@ export const CONTENTS_DIR = '../../';
  */
 export const CHAPTER_PATTERN = "^ch[0-9]{2}";
 export const SECTION_PATTERN = "^[0-9]{2}-";
+
+/**
+ * Target files
+ */
+export const TARGET_FILES = {
+    packageJson: "package.json",
+    typingsJson: "typings.json"
+}
+
+export const TEMPLATE_PACKAGE = './package.sample.json';
