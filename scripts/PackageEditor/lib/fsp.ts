92fbea6d5d7aad8a14eecdec64487c6f1d160f02
TonyPythoneer <tonypythoneer@gmail.com>
2016-09-19 12:11:59 +0800

    [ENHANCE] Add new functions when required

--- scripts/PackageEditor/lib/fsp.ts
+++ scripts/PackageEditor/lib/fsp.ts
@@ -2,10 +2,11 @@ import * as fs from 'fs';
 
 import * as Promise from 'bluebird';
 
-
 interface IFileSystemPromise {
     readdirAsync: (path: string) => Promise<string[]>,
     statAsync: (path: string) => Promise<fs.Stats>,
+    readFileAsync: (filename: string) => Promise<Buffer>,
+    writeFileAsync: (filename: string, data: any) => Promise<Buffer>,
 }
 
 const fsp = Promise.promisifyAll(fs) as IFileSystemPromise;
